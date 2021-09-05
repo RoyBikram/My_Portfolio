2b5a2cf55e77d156a9ae8e830d0845e023a13889
Bikram Roy <bikramroy364@gmail.com>
2021-09-05 15:36:02 +0530

    Add portfolio page and remove scrollbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,31 @@
     })
 })();
 
+
+
+// var swiper = new Swiper(".mySwiper", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     coverflowEffect: {
+//       rotate: 50,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 1,
+//       slideShadows: true,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+// });
+  
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    mousewheel: true,
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    //   },
+  });
