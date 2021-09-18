e38ff3032fcf60527e751d99c5d73e0893b07120
Bikram Roy <bikramroy364@gmail.com>
2021-09-18 13:34:44 +0530

    Redesign tech page and add active link funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,89 @@
-(() => {
-    const cursor = document.querySelector('.cursor');
-    const follower = document.querySelector('.follower');
-    const social_icons = document.querySelectorAll('.social_icons')
-    const nav_link = document.querySelectorAll('.nav_link')
-    const nav_massage = document.querySelector('.nav_massage')
-    const scroll_container = document.querySelector('.scroll-container')
-    const aboutpage_button = document.querySelector('.aboutpage_button')
-    const hire_button = document.querySelectorAll('.hire_button')
-    document.addEventListener('mousemove', e => {
-        cursor.style.left = e.pageX + 'px';
-        cursor.style.top = e.pageY + 'px';
-        setTimeout(() => {
-            follower.style.left = e.pageX + 'px';
-            follower.style.top = e.pageY + 'px';
-        }, 100);
+const slider_buttons = document.querySelectorAll('.slider_button')
+const cursor = document.querySelector('.cursor');
+const follower = document.querySelector('.follower');
+const social_icons = document.querySelectorAll('.social_icons img')
+const nav_link = document.querySelectorAll('.nav_link')
+const nav_list = document.querySelector('.nav_list')
+const nav_massage = document.querySelector('.nav_massage')
+const scroll_container = document.querySelector('.scroll-container')
+const aboutpage_button = document.querySelector('.aboutpage_button')
+const hire_button = document.querySelectorAll('.hire_button')
+const buttons = document.querySelectorAll('button')
+
+document.addEventListener('mousemove', e => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+    setTimeout(() => {
+        follower.style.left = e.pageX + 'px';
+        follower.style.top = e.pageY + 'px';
+    }, 100);
+})
+
+const list_for_hover = [...social_icons, ...nav_link,scroll_container,aboutpage_button,...hire_button,nav_massage,...buttons,...slider_buttons] 
+
+list_for_hover.forEach((each) => {
+    each.addEventListener('mouseenter', e => {
+        follower.classList.toggle('follower_enter')
     })
+    each.addEventListener('mouseleave', e => {
+        follower.classList.toggle('follower_enter')
 
-    const list_for_hover = [...social_icons, ...nav_link,scroll_container,aboutpage_button,...hire_button,nav_massage] 
-
-    list_for_hover.forEach((each) => {
-        each.addEventListener('mouseenter', e => {
-            follower.classList.toggle('follower_enter')
-        })
-        each.addEventListener('mouseleave', e => {
-            follower.classList.toggle('follower_enter')
-
-        })
     })
-})();
+})
 
-
-
-// var swiper = new Swiper(".mySwiper", {
-//     effect: "coverflow",
-//     grabCursor: true,
-//     centeredSlides: true,
-//     slidesPerView: "auto",
-//     coverflowEffect: {
-//       rotate: 50,
-//       stretch: 0,
-//       depth: 100,
-//       modifier: 1,
-//       slideShadows: true,
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//     },
-// });
-  
-var swiper = new Swiper(".mySwiper", {
+// Project page swiper
+var swiper = new Swiper("#project_slider", {
     effect: "cards",
     grabCursor: true,
     mousewheel: true,
-    autoplay: {
+    
+});
+
+// Tech page swiper
+
+var swiper = new Swiper("#Details_swiper", {
+  mousewheel: true,
+  autoplay: {
         delay: 5000,
-        disableOnInteraction: false,
+        disableOnInteraction: true,
       },
-  });
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+slider_buttons.forEach((each, index) => {
+each.addEventListener('click', (params) => {
+  swiper.slideTo(index,500)
+})
+})
+
+swiper.on('slideChange', function () {
+slider_buttons[swiper.previousIndex].classList.remove('slider_button_active')
+slider_buttons[swiper.activeIndex].classList.add('slider_button_active')
+})
+
+
+
+// Header active link funtion
+
+const sections = document.querySelectorAll('section')
+const header = document.querySelector('header')
+
+const options = {
+  threshold: 0.5
+}
+
+const observer = new IntersectionObserver((first, second) => {
+  first.forEach ((each) => {
+    if (each.isIntersecting) {
+      nav_list.querySelector(`.${each.target.className.slice(8, -8)}`).classList.add('active_link');
+    } else {
+      nav_list.querySelector(`.${each.target.className.slice(8, -8)}`).classList.remove('active_link');
+    }
+  })
+},options)
+sections.forEach(each => {
+  observer.observe(each)
+});
